66b7193cde1771c588372ee2329ae4453e5478af
Rayan Arnaout <arnaout@ebu.ch>
2019-11-12 13:16:59 +0100

    Updated gradle and configured jitpack distrib

--- app/src/androidTest/java/ch/ebu/peachdemo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ch/ebu/peachdemo/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("ch.ebu.peachmedo", appContext.getPackageName());
+        assertEquals("ch.ebu.peachdemo", appContext.getPackageName());
     }
 }
