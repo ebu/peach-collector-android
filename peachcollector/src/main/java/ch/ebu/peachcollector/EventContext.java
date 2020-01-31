733f54194d9ff9b7498ed5094610cbc51ce0ed2f
Rayan Arnaout <arnaout@ebu.ch>
2020-01-31 16:09:10 +0100

    Added possibility to create/retrieve/remove custom fields in context and properties

--- peachcollector/src/main/java/ch/ebu/peachcollector/EventContext.java
+++ peachcollector/src/main/java/ch/ebu/peachcollector/EventContext.java
@@ -58,6 +58,59 @@ public class EventContext {
         return context;
     }
 
+    @Nullable private Map<String, Object> customFields;
+
+    private void addObject(String key, Object value) {
+        if (value == null) {
+            remove(key);
+            return;
+        }
+
+        if (customFields == null) {
+            customFields = new HashMap<>();
+        }
+
+        customFields.put(key, value);
+    }
+
+    /**
+     * Add a custom string field to the context
+     */
+    public void add(String key, String value){
+        addObject(key, value);
+    }
+
+    /**
+     * Add a custom number field to the context
+     */
+    public void add(String key, Number value){
+        addObject(key, value);
+    }
+
+    /**
+     * Add a custom boolean field to the context
+     */
+    public void add(String key, Boolean value){
+        addObject(key, value);
+    }
+
+    /**
+     * Remove a custom field previously added
+     */
+    public void remove(String key){
+        if (customFields != null) customFields.remove(key);
+        if (customFields.size() == 0) customFields = null;
+    }
+
+    /**
+     * Retrieve a custom field previously added.
+     * @return null if the field was not found
+     */
+    @Nullable public Object get(String key){
+        if (customFields != null) return customFields.get(key);
+        return null;
+    }
+
     @Nullable
     Map<String, Object> jsonRepresentation() {
         Map<String, Object> json = new HashMap<>();
@@ -71,6 +124,11 @@ public class EventContext {
         if(referrer != null) { json.put(CONTEXT_REFERRER_KEY, referrer); }
         if(component != null && component.jsonRepresentation() != null) {
             json.put(CONTEXT_COMPONENT_KEY, component.jsonRepresentation());
+        }
+        if(customFields != null) {
+            for (Map.Entry<String, Object> entry : customFields.entrySet()) {
+                json.put(entry.getKey(), entry.getValue());
+            }
         }
         if (json.isEmpty()) return null;
         return json;
--- peachcollector/src/main/java/ch/ebu/peachcollector/EventProperties.java
+++ peachcollector/src/main/java/ch/ebu/peachcollector/EventProperties.java
@@ -80,6 +80,59 @@ public class EventProperties {
     @Nullable public String startMode;
 
 
+    @Nullable private Map<String, Object> customFields;
+
+    private void addObject(String key, Object value) {
+        if (value == null) {
+            remove(key);
+            return;
+        }
+
+        if (customFields == null) {
+            customFields = new HashMap<>();
+        }
+
+        customFields.put(key, value);
+    }
+
+    /**
+     * Add a custom string field to the properties
+     */
+    public void add(String key, String value){
+        addObject(key, value);
+    }
+
+    /**
+     * Add a custom number field to the properties
+     */
+    public void add(String key, Number value){
+        addObject(key, value);
+    }
+
+    /**
+     * Add a custom boolean field to the properties
+     */
+    public void add(String key, Boolean value){
+        addObject(key, value);
+    }
+
+    /**
+     * Remove a custom field previously added
+     */
+    public void remove(String key){
+        if (customFields != null) customFields.remove(key);
+        if (customFields.size() == 0) customFields = null;
+    }
+
+    /**
+     * Retrieve a custom field previously added.
+     * @return null if the field was not found
+     */
+    @Nullable public Object get(String key){
+        if (customFields != null) return customFields.get(key);
+        return null;
+    }
+
     @Nullable
     public Map<String, Object> jsonRepresentation() {
         Map<String, Object> json = new HashMap<>();
@@ -95,6 +148,11 @@ public class EventProperties {
         if(videoMode != null) { json.put(MEDIA_VIDEO_MODE_KEY, videoMode); }
         if(audioMode != null) { json.put(MEDIA_AUDIO_MODE_KEY, audioMode); }
         if(startMode != null) { json.put(MEDIA_START_MODE_KEY, startMode); }
+        if(customFields != null) {
+            for (Map.Entry<String, Object> entry : customFields.entrySet()) {
+                json.put(entry.getKey(), entry.getValue());
+            }
+        }
         if(json.isEmpty()) return null;
         return json;
     }
